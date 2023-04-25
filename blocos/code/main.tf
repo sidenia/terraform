caa65e0b43a9498e90121a300394d62b938fef62
sidenia <sideniabezerra@hotmail.com>
2023-04-24 22:13:00 -0300

    remocao coments

--- blocos/code/main.tf
+++ blocos/code/main.tf
@@ -2,8 +2,7 @@
 // estudando tipos e sintaxe de atributos para o bloco terraform
 
 terraform {
-    //////////////////////////////////////////////////////////////////////
-    //Parametro required_version
+    //Possibilidade de preenchimento Parametro required_version
 
     //usando versao especifica
     required_version = "1.3.0"
@@ -16,7 +15,7 @@ terraform {
     //pode rodar à partir da versão x e ir até 
     required_version = "~> 1.0.0"  // 1.0.0 <= x < 2.0.0 ou seja, até 1.0.n
 
-    ////////////////////////////////////////////////////////////////////////
+
     //Parametro required_provider 
     // é possível usar mais de 1 provider ao mesmo tempo
     required_providers {
@@ -31,8 +30,6 @@ terraform {
         }
     }
 
-
-    ////////////////////////////////////////////////////////////////////////
     // tipo de backend que vamos usar
         //configuracao especifica para cada tipo de backend que for utilizado
     backend "azurerm" {
